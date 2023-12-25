1ce91bc45716469a3d3c30d725afe05f942c1498
Poovizhi1991 <154439073+Poovizhi1991@users.noreply.github.com>
2023-12-25 15:10:36 +0530

    Add files via upload
    day2

---
+++ day2/day2/EdgeBrowser.java
@@ -0,0 +1,13 @@
+package Week1.day2;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		// object creation for a class Browser
+		Browser anotherBrowser = new Browser();
+		anotherBrowser.launchBrowser("Google");
+		anotherBrowser.loadUrl();
+
+	}
+
+}
