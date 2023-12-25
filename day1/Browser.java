a21f026f0c575a642c807d60eefd3e4dc7b1d399
Poovizhi1991 <154439073+Poovizhi1991@users.noreply.github.com>
2023-12-25 15:08:09 +0530

    Add files via upload
    day1

---
+++ day1/Browser.java
@@ -0,0 +1,15 @@
+package Week1.day1;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		//Print simple statement - Assignment 1
+		System.out.println("This is my Browser");
+		//Object declaration and method calling from other class - Assignment 2
+		Chrome myBrowser = new Chrome();
+		myBrowser.getName();
+		myBrowser.printName();
+
+	}
+
+}
---
+++ day1/Car.java
@@ -0,0 +1,14 @@
+package Week1.day1;
+
+public class Car {
+	//Two objects for two classes - Assignment 4
+	public void applyBreak()
+	{
+		System.out.println("Applied break.");
+	}
+	public void soundHorn()
+	{
+		System.out.println("Sound horn.");
+	}
+
+}
