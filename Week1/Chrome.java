f8a7a7e23cf29cc9777892c8d96a054d1bbf6b55
Poovizhi1991 <154439073+Poovizhi1991@users.noreply.github.com>
2024-01-03 22:00:28 +0530

    Add files via upload

---
+++ Week1/Chrome.java
@@ -0,0 +1,16 @@
+package Week1.day1;
+
+public class Chrome {
+	//Method Declaration - Assignment 3 
+	public void getName()
+	{
+		
+	}
+	public void printName()
+	{
+		
+		System.out.println("This is google chrome.");
+	}
+
+}
+
