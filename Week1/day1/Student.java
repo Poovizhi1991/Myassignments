5f17aa47d3d5a38197e13230b711b20260140e2a
Poovizhi1991 <154439073+Poovizhi1991@users.noreply.github.com>
2024-01-03 21:45:32 +0530

    Day1

---
+++ Week1/day1/Student.java
@@ -0,0 +1,11 @@
+package Week1.day1;
+
+public class Student {
+	// Variable declaration - Assignment 5
+	String studentName = "Jayachandran";
+	String rollNo="SN2356";
+	String collageName ="SNS Collage of Engineering";
+	int markScored=445;
+	float cgpa=89.321f;
+
+	}
